977e94d3a7e75e041f9c1b6a587df4e889306276
Khaled Hammouda <khaledh@gmail.com>
2022-07-11 21:51:57 -0400

    move shell code to shell module

--- src/boot.nim
+++ src/boot.nim
@@ -1,41 +1,30 @@
-import std/strbasics
 import std/strformat
-import std/strutils
 import std/tables
 
-import acpi
-import acpi/fadt
-import acpi/madt
-import acpi/xsdt
-import ahci
+import acpi, acpi/[fadt, madt, xsdt]
 import bxvbe
 import console
 import cpu
 import debug
-import firmware
 import font
 import framebuffer
-import gdt
 import idt
-import interrupt
 import ioapic
 import keyboard
 import lapic
-import paging
-import pci
-import physmem
 import shell
 import task
 import timer
-import uefi
-import uefitypes
-import uefi/gop
-import uefi/simpletext
-import lib/libc
-import lib/malloc
-import lib/guid
+import uefi, uefitypes
+import lib/[libc, malloc]
 
-var sysTable: ptr EfiSystemTable
+var
+  sysTable: ptr EfiSystemTable
+  rsdp: ptr Rsdp
+  xsdt0: Xsdt
+  fadt0: ptr Fadt
+  madt0: ptr Madt
+  ioapic0: ioapic.Ioapic
 
 proc printError(msg: string) =
   writeln(msg)
@@ -43,118 +32,6 @@ proc printError(msg: string) =
 proc handleUnhandledException(e: ref Exception) {.tags: [], raises: [].} =
   printError(e.msg)
 
-
-proc thread1() {.cdecl.} =
-  # while true:
-  for i in 0..<20:
-    write(".")
-    for i in 0..250000:
-      asm "pause"
-
-proc thread2() {.cdecl.} =
-  # while true:
-  for i in 0..<20:
-    write("o")
-    for i in 0..250000:
-      asm "pause"
-
-var
-  rsdp: ptr Rsdp
-  xsdt0: Xsdt
-  fadt0: ptr Fadt
-  madt0: ptr Madt
-  ioapic0: ioapic.Ioapic
-  lineBuffer: string
-
-proc showHelp() =
-  writeln("Commands")
-  writeln("")
-  writeln("  ping          Respond with 'pong'")
-  writeln("  firmware      Show firmware version")
-  writeln("  memory        Show the memory map")
-  writeln("  uefi tables   Show UEFI config table")
-  writeln("  uefi text     Show UEFI text modes")
-  writeln("  rsdp          Show ACPI RSDP")
-  writeln("  xsdt          Show ACPI XSDT table")
-  writeln("  fadt          Show ACPI FADT table")
-  writeln("  madt          Show ACPI MADT table")
-  writeln("  idt           Show Interrupt Descriptor Table")
-  writeln("  gdt           Show Global Descriptor Table")
-  writeln("  lapic         Show Local APIC information")
-  writeln("  iopic         Show I/O APIC information")
-  writeln("  pci           Show PCI configuration")
-  writeln("  ahci          Show AHCI configuration")
-  writeln("  cpuid         Show CPUID information")
-  writeln("  ctlreg        Show CPU control registers")
-  writeln("  font          Show font information")
-  writeln("  halt          Halt without shutting down")
-  writeln("  shutdown      Shutdown")
-
-proc dispatchCommand(cmd: string) =
-  case cmd:
-  of "help":
-    showHelp()
-  of "ping":
-    writeln("pong")
-  of "rsdp":
-    showRsdp(rsdp)
-  of "xsdt":
-    showXsdt(xsdt0)
-  of "fadt":
-    showFadt(fadt0)
-  of "madt":
-    showMadt(madt0)
-  of "idt":
-    showIdt()
-  of "gdt":
-    showGdt()
-  of "lapic":
-    lapic.show()
-  of "ioapic":
-    ioapic0.show()
-  of "pci":
-    showPciConfig()
-  of "ahci":
-    showAhci(bus=0, dev=0x1f, fn=2, sysTable.bootServices)
-  of "font":
-    showFont()
-  of "cpuid":
-    showCpuid()
-  of "ctlreg":
-    showControlRegisters()
-  of "memory":
-    discard showMemoryMap(sysTable.bootServices)
-  of "paging":
-    showPagingTables()
-  of "firmware":
-    showFirmwareVersion(sysTable)
-  of "uefi tables":
-    showUefiConfigTables(sysTable)
-  of "uefi text":
-    showSimpletext(sysTable.conOut)
-  of "shutdown":
-    writeln("Shutting down")
-    shutdown()
-  of "halt":
-    writeln("Halt")
-    halt()
-  else:
-    writeln("Uknown command")
-
-proc keyHandler(evt: KeyEvent) =
-  if evt.eventType == KeyDown and evt.ch != '\0':
-    if evt.ch == '\n':
-      writeln("")
-      dispatchCommand(lineBuffer)
-      writeln("")
-      write("] ")
-      lineBuffer = ""
-    elif evt.ch == '\b':
-      lineBuffer.delete(len(lineBuffer)-1 .. len(lineBuffer)-1)
-      write(&"{evt.ch}")
-    else:
-      lineBuffer &= evt.ch
-      write(&"{evt.ch}")
 
 proc efiMain*(imageHandle: EfiHandle, systemTable: ptr EfiSystemTable): uint {.exportc.} =
   sysTable = systemTable
--- src/shell.nim
+++ src/shell.nim
@@ -13,7 +13,29 @@
     - shell
 ]#
 
+import std/[strformat, strutils]
+
+import acpi, acpi/[fadt, madt, xsdt]
+import ahci
 import console
+import cpu
+import firmware
+import gdt
+import idt
+import ioapic
+import keyboard
+import lapic
+import paging
+import pci
+import physmem
+import uefi, uefi/simpletext, uefitypes
+
+var
+  sysTable: ptr EfiSystemTable
+  lineBuffer: string
+
+proc init*(st: ptr EfiSystemTable) =
+    sysTable = st
 
 proc start*() {.cdecl.} =
 
@@ -27,3 +49,122 @@ proc start*() {.cdecl.} =
         # dispatch
 
     discard
+
+proc dispatchCommand(cmd: string)
+
+proc keyHandler*(evt: KeyEvent) =
+  if evt.eventType == KeyDown and evt.ch != '\0':
+    if evt.ch == '\n':
+      writeln("")
+      dispatchCommand(lineBuffer)
+      writeln("")
+      write("] ")
+      lineBuffer = ""
+    elif evt.ch == '\b':
+      lineBuffer.delete(len(lineBuffer)-1 .. len(lineBuffer)-1)
+      write(&"{evt.ch}")
+    else:
+      lineBuffer &= evt.ch
+      write(&"{evt.ch}")
+
+proc showHelp() =
+  writeln("")
+  writeln("UEFI")
+  writeln("  uefi fw       Show UEFI firmware version")
+  writeln("  uefi memory   Show UEFI memory map")
+  writeln("  uefi tables   Show UEFI config table")
+  # writeln("  uefi text     Show UEFI text modes")
+
+  writeln("")
+  writeln("CPU")
+  writeln("  cpuid         Show CPUID information")
+  writeln("  ctlreg        Show CPU control registers")
+  writeln("  gdt           Show GDT (Global Descriptor Table)")
+  writeln("  idt           Show IDT (Interrupt Descriptor Table)")
+  writeln("  paging        Show paging table")
+
+  writeln("")
+  writeln("ACPI")
+  writeln("  rsdp          Show ACPI RSDP (Root System Description Pointer)")
+  writeln("  xsdt          Show ACPI XSDT (eXtended System Description Table)")
+  writeln("  fadt          Show ACPI FADT (Fixed ACPI Description Table)")
+  writeln("  madt          Show ACPI MADT (Multiple APIC Description Table)")
+
+  writeln("")
+  writeln("Interrupt Controllers")
+  writeln("  lapic         Show Local APIC information")
+  writeln("  iopic         Show I/O APIC information")
+
+  writeln("")
+  writeln("PCI Bus")
+  writeln("  pci           Show PCI configuration")
+
+  writeln("")
+  writeln("Storage")
+  writeln("  ahci          Show AHCI configuration")
+
+  writeln("")
+  writeln("Other")
+  writeln("  about         Information about Axiom OS")
+  writeln("  ping          Respond with 'pong'")
+  writeln("  font          Show font information")
+  writeln("  halt          Halt without shutting down")
+  writeln("  shutdown      Shutdown the system")
+  writeln("  bye           Alias to 'shutdown'")
+
+proc showAbout() =
+  writeln("Axiom OS")
+  writeln("(c) 2022 Khaled Hammouda <khaledh@gmail.com>")
+
+proc dispatchCommand(cmd: string) =
+  case cmd:
+  of "help":
+    showHelp()
+  of "about":
+    showAbout()
+  of "ping":
+    writeln("pong")
+#   of "rsdp":
+#     showRsdp(rsdp)
+#   of "xsdt":
+#     showXsdt(xsdt0)
+#   of "fadt":
+#     showFadt(fadt0)
+#   of "madt":
+#     showMadt(madt0)
+  of "idt":
+    showIdt()
+  of "gdt":
+    showGdt()
+  of "lapic":
+    lapic.show()
+#   of "ioapic":
+#     ioapic0.show()
+  of "pci":
+    showPciConfig()
+  of "ahci":
+    showAhci(bus=0, dev=0x1f, fn=2, sysTable.bootServices)
+  of "font":
+    showFont()
+  of "cpuid":
+    showCpuid()
+  of "ctlreg":
+    showControlRegisters()
+  of "uefi memory":
+    discard showMemoryMap(sysTable.bootServices)
+  of "paging":
+    showPagingTables()
+  of "uefi fw":
+    showFirmwareVersion(sysTable)
+  of "uefi tables":
+    showUefiConfigTables(sysTable)
+  of "uefi text":
+    showSimpletext(sysTable.conOut)
+  of "shutdown", "bye":
+    writeln("Shutting down")
+    shutdown()
+  of "halt":
+    writeln("Halt")
+    halt()
+  else:
+    writeln("Uknown command")
