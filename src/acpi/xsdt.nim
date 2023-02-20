938b01fa3c98e97c7a9c2bd6776c657945053b1f
Khaled Hammouda <khaledh@gmail.com>
2023-02-20 14:55:09 -0500

    experiment: use binarylang to parse system tables

--- src/acpi/xsdt.nim
+++ src/acpi/xsdt.nim
@@ -1,5 +1,5 @@
 #[
-  ACPI: XSDt0 (eXtended System Description Table)
+  ACPI: XSDT (eXtended System Description Table)
 
   Responsibilities:
   - ???
@@ -14,45 +14,57 @@
 import std/options
 import std/strformat
 
-import ../console
 import table
+import ../console
+import ../lib/memstream
 
-type
-  Xsdt* = ref object
-    hdr: ptr TableDescriptionHeader
-    numEntries: int
+
+import binarylang
+import bitstreams
+
+struct(tableDescHeader, endian = l):
+  8: signature[4]
+  32: length
+  8: revision
+  8: checksum
+  s: oemId(6)
+  s: oemTableId(8)
+  32: oemRevision
+  s: creatorId(4)
+  32: creatorRevision
+
+struct(xsdt, endian = l):
+  *tableDescHeader: hdr
+  64: entries[(hdr.length - s.getPosition) div 8]
 
 var
   xsdt0: Xsdt
 
+proc newMemoryBitStream(buf: pointer, bufLen: int): BitStream =
+  var p = cast[ptr UncheckedArray[byte]](buf)
+  result = BitStream(stream: newMemoryStream(p, bufLen))
+
 
 proc initXsdt*(xsdtAddr: pointer) =
-  xsdt0 = new(Xsdt)
-  let hdr = cast[ptr TableDescriptionHeader](xsdtAddr)
-  xsdt0.hdr = hdr
-  xsdt0.numEntries = (hdr.length.int - sizeof(TableDescriptionHeader)) div 8
-
-
+  var bs = newMemoryBitStream(xsdtAddr, 100)
+  xsdt0 = xsdt.get(bs)
 
 iterator entries(): ptr TableDescriptionHeader =
-  for i in 0 ..< xsdt0.numEntries:
-    let tablePtrLoc = cast[ptr uint64](cast[int](xsdt0.hdr) + sizeof(TableDescriptionHeader) + i.int * 8)
-    let tableHdr = cast[ptr TableDescriptionHeader](tablePtrLoc[])
+  for i in 0 ..< xsdt0.entries.len:
+    let tableHdr = cast[ptr TableDescriptionHeader](xsdt0.entries[i])
     yield tableHdr
- 
 
 proc findBySignature*(sig: array[4, char]): Option[ptr TableDescriptionHeader] =
   for hdr in entries():
     if hdr.signature == sig:
       return some(hdr)
 
-
 proc showXsdt*() =
   writeln("")
   writeln("XSDT")
   writeln(&"  Address: {cast[uint64](xsdt0.hdr):8>x}h")
   writeln(&"  Revision: {xsdt0.hdr.revision}h")
-  writeln(&"  Number of Entries: {xsdt0.numEntries}")
+  writeln(&"  Number of Entries: {xsdt0.entries.len}")
   writeln("")
 
   for hdr in entries():
---
+++ src/lib/memstream2.nim
@@ -0,0 +1,78 @@
+import std/streams
+
+type
+  MemoryStream* = ref MemoryStreamObj
+  MemoryStreamObj* = object of StreamObj
+    data*: ptr UncheckedArray[byte]
+    pos: int
+    len: int
+
+
+proc msAtEnd(s: Stream): bool =
+  var s = MemoryStream(s)
+  return s.pos >= s.data.len
+
+proc msSetPosition(s: Stream, pos: int) =
+  var s = MemoryStream(s)
+  s.pos = clamp(pos, 0, s.data.len)
+
+proc msGetPosition(s: Stream): int =
+  var s = MemoryStream(s)
+  return s.pos
+
+proc msReadDataStr(s: Stream, buffer: var string, slice: Slice[int]): int =
+  var s = MemoryStream(s)
+  when declared(prepareMutation):
+    prepareMutation(buffer) # buffer might potentially be a CoW literal with ARC
+  result = min(slice.b + 1 - slice.a, s.data.len - s.pos)
+  if result > 0:
+    copyMem(addr buffer[slice.a], addr s.data[s.pos], result)
+    inc(s.pos, result)
+  else:
+    result = 0
+
+proc msReadData(s: Stream, buffer: pointer, bufLen: int): int =
+  var s = MemoryStream(s)
+  result = min(bufLen, s.data.len - s.pos)
+  if result > 0:
+    copyMem(buffer, addr(s.data[s.pos]), result)
+    inc(s.pos, result)
+  else:
+    result = 0
+
+proc msPeekData(s: Stream, buffer: pointer, bufLen: int): int =
+  var s = MemoryStream(s)
+  result = min(bufLen, s.data.len - s.pos)
+  if result > 0:
+    copyMem(buffer, addr(s.data[s.pos]), result)
+  else:
+    result = 0
+
+proc msWriteData(s: Stream, buffer: pointer, bufLen: int) =
+  var s = MemoryStream(s)
+  if bufLen <= 0:
+    return
+  if s.pos + bufLen > s.data.len:
+    setLen(s.data, s.pos + bufLen)
+  copyMem(addr(s.data[s.pos]), buffer, bufLen)
+  inc(s.pos, bufLen)
+
+proc msClose(s: Stream) =
+  var s = MemoryStream(s)
+  s.data = nil
+
+proc newMemoryStream*(p: sink ptr UncheckedArray[uint8]): owned MemoryStream =
+  ## Creates a new stream from the memory pointed to by `p`.
+  new(result)
+  result.data = p
+  when declared(prepareMutation):
+    prepareMutation(result.data) # Allows us to mutate using `addr` logic like `copyMem`, otherwise it errors.
+  result.pos = 0
+  result.closeImpl = msClose
+  result.atEndImpl = msAtEnd
+  result.setPositionImpl = msSetPosition
+  result.getPositionImpl = msGetPosition
+  result.readDataStrImpl = msReadDataStr
+  result.readDataImpl = msReadData
+  result.peekDataImpl = msPeekData
+  result.writeDataImpl = msWriteData
