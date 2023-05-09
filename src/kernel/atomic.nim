517743ec795e2afa2706a42918eb8477f53e78a6
Khaled Hammouda <khaledh@gmail.com>
2023-05-08 21:08:02 -0400

    more robust cmpxchg

--- src/kernel/atomic.nim
+++ src/kernel/atomic.nim
@@ -1,8 +1,7 @@
 proc cmpxchg*(toChange: var bool, oldValue: bool, newValue: bool): bool {.inline.} =
   asm """
-    lock ; cmpxchg %0, %2
-    : "+m"(*`toChange`), "+a"(`oldValue`)
-    : "r"(`newValue`)
+    lock cmpxchg %0, %3
+    : "+m"(*`toChange`), "=a"(`result`)
+    : "a"(`oldValue`), "r"(`newValue`)
     : "memory"
   """
-  result = oldValue
