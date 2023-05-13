3051192afa99a9a30647d5073e214ef46411f2e8
Khaled Hammouda <khaledh@gmail.com>
2023-05-13 12:19:47 -0400

    cmpxchg: fix use of register

--- src/kernel/atomic.nim
+++ src/kernel/atomic.nim
@@ -2,7 +2,7 @@ proc cmpxchg*(location: var bool, expected: bool, newval: bool): bool {.inline.}
   asm """
     lock cmpxchg %0, %3
     sete %1
-    : "+m"(*`location`), "=a"(`result`)
+    : "+m"(*`location`), "=q"(`result`)
     : "a"(`expected`), "r"(`newval`)
     : "memory"
   """
